90533298902657df4f2c5dabb82126b7248360b8
Andy Griffiths <Andrew.Griffiths@airnz.co.nz>
2013-07-05 07:49:16 +1200

    Added function to get keys

--- test/muso_test.js
+++ test/muso_test.js
@@ -10,8 +10,23 @@ exports['notes'] = {
   'correctly matches array of notes': function(notes) {
     notes.expect(1);
     // tests here
-    var allNotes=new Array("A","A#","B","C","C#","D","D#","E","F","F#","G","G#");
+    var allNotes=new Array("C","C#","D","D#","E","F","F#","G","G#","A","A#","B");
     notes.deepEqual(muso.notes(), allNotes,'notes are incorrect');
     notes.done();
+  }
+
+};
+
+exports['keys'] = {
+  setUp: function(done) {
+    // setup here
+    done();
   },
+  'correctly gives all keys': function(keys) {
+    keys.expect(1);
+    // tests here
+    var allKeys=new Array("C", "G", "D", "A", "E", "B", "F#", "C#","G#", "D#", "A#", "F");
+    keys.deepEqual(muso.keys(), allKeys,'keys are incorrect');
+    keys.done();
+  }
 };
