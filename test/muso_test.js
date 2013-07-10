8218fa73a12d27e0c11b7ba4af48507e29504e9d
Andy Griffiths <Andrew.Griffiths@airnz.co.nz>
2013-07-11 07:59:58 +1200

    Some refactoring

--- test/muso_test.js
+++ test/muso_test.js
@@ -3,40 +3,40 @@
 var muso = require('../lib/muso.js');
 var _ = require('underscore');
 
-exports['notes'] = {
+exports.notes = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'correctly matches array of notes': function(notes) {
-    notes.expect(1);
+  'correctly matches array of notes': function(test) {
+    test.expect(1);
     // tests here
     var allNotes=new Array("C","C#","D","D#","E","F","F#","G","G#","A","A#","B");
-    notes.deepEqual(muso.notes(), allNotes,'notes are incorrect');
-    notes.done();
+    test.deepEqual(muso.notes(), allNotes,'notes are incorrect');
+    test.done();
   }
 };
 
-exports['keys'] = {
+exports.keys = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'correctly gives all keys': function(keys) {
-    keys.expect(1);
+  'correctly gives all keys': function(test) {
+    test.expect(1);
     // tests here
     var allKeys=new Array("C", "G", "D", "A", "E", "B", "F#", "C#","G#", "D#", "A#", "F");
-    keys.deepEqual(muso.keys(), allKeys,'keys are incorrect');
-    keys.done();
+    test.deepEqual(muso.keys(), allKeys,'keys are incorrect');
+    test.done();
   }
 };
-exports['enharmonic'] = {
+exports.enharmonic = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'correctly gives enharmonic equivalents': function(enharmonic) {
-    enharmonic.expect(1);
+  'correctly gives enharmonic equivalents': function(test) {
+    test.expect(1);
 
     // tests here
     var expectedNotes=new Array("C#","D#","F#","G#","A#","Db","Eb","Gb","Ab","Bb","A","C");
@@ -44,17 +44,17 @@ exports['enharmonic'] = {
     var notesToTest=new Array("Db","Eb","Gb","Ab","Bb","C#","D#","F#","G#","A#","A","C");
 
     var actualNotes = _.map(notesToTest, function(note){ return muso.enharmonic(note); });
-    enharmonic.deepEqual(actualNotes, expectedNotes,'enharmonic notes are incorrect');
-    enharmonic.done();
+    test.deepEqual(actualNotes, expectedNotes,'enharmonic notes are incorrect');
+    test.done();
   }
 };
-exports['scale'] = {
+exports.scale = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'correctly gives major scale notes': function(scale) {
-    scale.expect(2);
+  'correctly gives major scale notes': function(test) {
+    test.expect(2);
 
     // tests here
 
@@ -63,75 +63,75 @@ exports['scale'] = {
 	var gKey = "G";
 
     var gScale = muso.major(gKey);
-    scale.deepEqual(gScale, gExpectedNotes,'G Major scale is incorrect');
+    test.deepEqual(gScale, gExpectedNotes,'G Major scale is incorrect');
 	
     // C MAJOR
     var cExpectedNotes=new Array("C","D","E","F","G","A","B");
 	var cKey = "C";
 
     var cScale = muso.major(cKey);
-    scale.deepEqual(cScale, cExpectedNotes,'C Major scale is incorrect');
+    test.deepEqual(cScale, cExpectedNotes,'C Major scale is incorrect');
 
 
-    scale.done();
+    test.done();
   },
-  'correctly gives minor scale notes': function(scale) {
-    scale.expect(2);
+  'correctly gives minor scale notes': function(test) {
+    test.expect(2);
 
     // a minor 
     var aExpectedNotes=new Array("A","B","C","D","E","F","G");
 	var aKey = "A";
 
     var aScale = muso.minor(aKey);
-    scale.deepEqual(aScale,aExpectedNotes,'A Minor scale is incorrect');
+    test.deepEqual(aScale,aExpectedNotes,'A Minor scale is incorrect');
 
     // c minor 
     var cExpectedNotes=new Array("C","D","Eb","F","G","Ab","Bb");
 	var cKey = "C";
 
     var cScale = muso.minor(cKey);
-    scale.deepEqual(cScale,cExpectedNotes,'C Minor scale is incorrect');
+    test.deepEqual(cScale,cExpectedNotes,'C Minor scale is incorrect');
 
-    scale.done();
+    test.done();
   },
 };
-exports['chords'] = {
+exports.chords = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'correctly gives chords for key': function(chords) {
-    chords.expect(2);
+  'correctly gives chords for key': function(test) {
+    test.expect(2);
 
 	// c Major 
     var cExpectedChords=new Array("Cmaj","Dmin","Emin","Fmaj","Gmaj","Amin","Bdim");
 	var cKey = "Cmaj";
 
     var cChords = muso.chords(cKey);
-    chords.deepEqual(cChords,cExpectedChords,'C Major chords are incorrect');
+    test.deepEqual(cChords,cExpectedChords,'C Major chords are incorrect');
 
 	// e minor 
     var eExpectedChords=new Array("Emin","F#dim","Gmaj","Amin","Bmin","Cmaj","Dmaj");
 	var eKey = "Emin";
 
     var eChords = muso.chords(eKey);
-    chords.deepEqual(eChords,eExpectedChords,'E minor chords are incorrect');
-    chords.done();
+    test.deepEqual(eChords,eExpectedChords,'E minor chords are incorrect');
+    test.done();
   }
 };
-exports['sequence'] = {
+exports.sequence = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'gives a sequence of defined length': function(sequence) {
-    sequence.expect(1);
+  'gives a sequence of defined length': function(test) {
+    test.expect(1);
 
 	var expectedLength = 8;
 	var aKey = "Cmaj";
 	var seq = muso.sequence(aKey,expectedLength);
-	sequence.strictEqual(seq.length, expectedLength, "Sequence is incorrect");
-    sequence.done();
+	test.strictEqual(seq.length, expectedLength, "Sequence is incorrect");
+    test.done();
   }
 };
 
